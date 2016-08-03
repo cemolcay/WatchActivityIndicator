1c7e26f1e8e0bbb036f708d73a828179cce023a5
cemolcay <ccemolcay@gmail.com>
2016-08-02 22:47:48 -0700

    Initial Commit

---
+++ WatchActivityIndicator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchActivityIndicator
+//
+//  Created by Cem Olcay on 02/08/16.
+//  Copyright © 2016 Prototapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
